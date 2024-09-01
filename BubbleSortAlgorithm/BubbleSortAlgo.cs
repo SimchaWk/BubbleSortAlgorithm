b257bbe16f4a3257d4c8117f937db6f44d5a6abe
Simcha <Simchav0@gmail.com>
2024-09-01 10:50:09 +0300

    Initial commit

---
+++ BubbleSortAlgorithm/BubbleSortAlgo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortAlgorithm
+{
+    internal class BubbleSortAlgo
+    {
+        public static int[] BubbleSort(int[] arr)
+        {
+            bool unSorted = true;
+            int tempLength = arr.Length - 1;
+
+            while (unSorted)
+            {
+                unSorted = false;
+                for (int i = 0; i < tempLength; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        (arr[i], arr[i + 1]) = (arr[i + 1], arr[i]);
+                        unSorted = true;
+                    }
+                }
+                tempLength--;
+            }
+            return arr;
+        }
+    }
+}
---
+++ BubbleSortAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using static BubbleSortAlgorithm.BubbleSortAlgo;
+
+namespace BubbleSortAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 8, 3, 2, 6, 5, 4, 3, };
+            Console.WriteLine(string.Join(", ", nums));
+
+
+            Console.WriteLine(string.Join(", ", BubbleSort(nums)));
+        }
+    }
+}
